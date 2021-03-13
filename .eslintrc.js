d9e3d641d98fcc27ca5804453707df289fd1a554
wangxingkang <wang_xingkang@qq.com>
2021-03-13 10:42:27 +0800

    🚧 wip: add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: ['alloy', 'alloy/react', 'alloy/typescript'],
+  env: {
+    browser: true,
+    jest: true
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  globals: {},
+  rules: {}
+};
