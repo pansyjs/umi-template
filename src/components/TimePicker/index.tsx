afee4951d770945cebef99405d45cef944c5e31d
wangxingkang <wang_xingkang@qq.com>
2021-03-14 22:20:39 +0800

    🚨 lint: 修复eslint警告

--- src/components/TimePicker/index.tsx
+++ src/components/TimePicker/index.tsx
@@ -4,7 +4,7 @@ import DatePicker from '../DatePicker';
 import { PickerTimeProps } from 'antd/es/date-picker/generatePicker';
 import { Omit } from 'antd/es/_util/type';
 
-export interface TimePickerProps extends Omit<PickerTimeProps<Dayjs>, 'picker'> {}
+export type TimePickerProps = Omit<PickerTimeProps<Dayjs>, 'picker'>;
 
 const TimePicker = React.forwardRef<any, TimePickerProps>((props, ref) => {
   return <DatePicker {...props} picker="time" mode={undefined} ref={ref} />;
